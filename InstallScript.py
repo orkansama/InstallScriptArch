c4bb7ae1a5e9dca6433153e83482217588449ad8
OrkanGoekcen <orkan.gok@gmail.com>
2026-09-18 15:12:28 +0200

    more progress

--- InstallScript.py
+++ InstallScript.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+import subprocess
 
 # Install Packages
-Path.()
+subprocess.call("echo $EDITOR", shell=True)
+subprocess.call("sudo pacman -S < packages.txt", shell=True)
 
 # Setup Dotfiles
 
